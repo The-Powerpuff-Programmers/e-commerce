7a379f489aff3a547b7ad4128075de02b35d7c06
Felicia Heiney <bhfelicia@gmail.com>
2020-07-01 12:31:23 -0400

    cleans up e-commerce site

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -6,26 +6,25 @@ import {logout} from '../store'
 
 const Navbar = ({handleClick, isLoggedIn}) => (
   <div>
-    <h1 className="siteHeader"> 🎨 Artsy Posters 🎨</h1>
-    <h3 className="siteHeader"> ~ Cover Your Walls in Culture ~ </h3>
+    <h1 className="siteHeader">Artsy Posters</h1>
     <nav>
       {isLoggedIn ? (
         <div className="navDiv">
           {/* The navbar will show these links after you log in */}
-          <Link to="/home">Home 🏠</Link>
+          <Link to="/home">Home </Link>
           <a href="#" onClick={handleClick}>
-            Logout 👋
+            Logout
           </a>
-          <Link to="/products">Peruse our Products 🧐</Link>
-          <Link to="/cart">View Cart 🛒</Link>
+          <Link to="/products">Products</Link>
+          <Link to="/cart">View Cart</Link>
         </div>
       ) : (
         <div>
           {/* The navbar will show these links before you log in */}
           <Link to="/login">Login</Link>
           <Link to="/signup">Sign Up</Link>
-          <Link to="/products">Peruse our Products 🧐</Link>
-          <Link to="/guestCart">View Cart 🛒</Link>
+          <Link to="/products">Products</Link>
+          <Link to="/guestCart">View Cart</Link>
         </div>
       )}
     </nav>
